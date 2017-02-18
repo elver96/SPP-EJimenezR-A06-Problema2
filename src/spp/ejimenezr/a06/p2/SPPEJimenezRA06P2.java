3690583b2a7b6f88404e319f995c0cfadfa0b5ae
elver <elver@192.168.1.75>
2017-02-17 20:21:20 -0600

    Actividad 06 problema 1

---
+++ src/spp/ejimenezr/a06/p2/SPPEJimenezRA06P2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spp.ejimenezr.a06.p2;
+import java.util.Scanner;
+/**
+ *
+ * @author elver
+ */
+public class SPPEJimenezRA06P2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int opEntrada;
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Desea realizar una sumatoria de numeros positivos? 1=Si, 2=No");
+        opEntrada=kb.nextInt();
+    }
+    static double calcularFactorial(double num){
+        for(){
+            
+        } //For
+    } //Metodo
+}
